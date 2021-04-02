8b31cb128121a20b60a03145b6677d3b1a45714e
cglang <adolphlanglang@163.com>
2021-04-03 05:04:33 +0800

    2021-04-03 05:04:31

--- iris/dist/js/garden.js
+++ iris/dist/js/garden.js
@@ -1,17 +1,30 @@
+window.onload = function () {
+    let route = GetRoute();
+    let path = GetPathByRoute(route);
+    RouterBeforeEach(path);
+}
 
 // href 变化时
-window.onhashchange = function () {
-    let href = window.location.href;
-    let index = href.lastIndexOf("#");
-    let route = href.substring(index + 1, href.length);
-    //let menu = document.querySelector("a[url='" + href + "']");    
+window.onhashchange = () => {
+    let route = GetRoute();
     let path = GetPathByRoute(route);
     RouterBeforeEach(path);
 };
 
 // 修改 href
 function ChangeRouter(path, kvs) {
-    location.href = location.href.replace(/#.*/g, "") + "#" + path + "?" + kvs;
+    if (kvs)
+        location.href = location.href.replace(/#.*/g, "") + "#" + path + "?" + kvs;
+    location.href = location.href.replace(/#.*/g, "") + "#" + path
+}
+
+function GetRoute() {
+    var href = window.location.href;
+    let index = href.lastIndexOf("#");
+    if (index === -1 || index + 1 === href.length) {
+        return "/"
+    }
+    return href.substring(index + 1, href.length);
 }
 
 // 通过路由找到path
@@ -38,8 +51,64 @@ function AjaxGet(pageurl, then) {
 
 
 // 路由拦截器
-function RouterBeforeEach(path) {
-    AjaxGet(path, (html) => {
-        document.getElementById("app").innerHTML = html;
-    });
+function RouterBeforeEach(route) {
+    if (route === "/") {
+        AjaxGet("pages/dashboard/", (html) => {
+            $("#app").html(html);
+        });
+    } else {
+        let path = GetPathByRoute(route);
+        AjaxGet(path, (html) => {
+            $("#app").html(html);
+        });
+    }
+}
+
+
+var mh = `<li class="nav-header">{headname}</li>`;
+var mi = `<li class="nav-item"><a href="javascript:;" class="nav-link" url="{url}" onclick="MenuClick(this)"><i class="nav-icon fas {ico}"></i><p>{name}</p></a></li>`
+var mt = `<li class="nav-item"><a href="javascript:;" class="nav-link"><i class="nav-icon fas {ico}"></i><p>{name}<i class="right fas fa-angle-left"></i></p></a><ul class="nav nav-treeview">{link}</ul></li>`
+
+AjaxGet(globalConfig.sidebarapi, (data) => {
+    InitMenu(data);
+});
+
+function InitMenu(data) {
+    data.forEach((element) => {
+        var header = element.group ? mh.replace("{headname}", element.group) : "";
+        var items = Menu(element.items);
+
+        let box = document.createElement('div');
+        let frag = document.createDocumentFragment();
+        box.innerHTML = header + items;
+        while (box.firstElementChild) {
+            frag.appendChild(box.firstElementChild);
+        }
+        document.getElementById("sidebar").appendChild(frag);
+    })
+}
+
+function Menu(items) {
+    var htmlitems = "";
+    items.forEach((item) => {
+        if (item.items) {
+            html = mt.replace("{url}", item.url).replace("{ico}", item.ico).replace("{name}", item.name);
+            htmlitems += html.replace("{link}", Menu(item.items));
+        } else {
+            htmlitems += mi.replace("{url}", item.url).replace("{ico}", item.ico).replace("{name}", item.name);
+        }
+    })
+    return htmlitems;
+}
+
+function MenuClick(a) {
+    if (a.getAttribute("class").search("active") === -1) {
+        ChangeRouter(a.getAttribute("url"));
+        UpdateSidebarStyle(a.getAttribute("url"));
+    }
+}
+
+function UpdateSidebarStyle(route) {
+    $(".nav-link.active").removeClass("active");
+    document.querySelector("a[url='" + route + "']").classList.add("active");
 }
